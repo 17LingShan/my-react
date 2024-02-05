3be3efc758ca4365b291062b7cfa0e6431fa9ed7
17LingShan <huangruofeng9@gmail.com>
2024-02-05 22:15:56 +0800

    :rocket:

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
-import "./index.css";
-import Ling, { createElement } from "./packages/Ling";
+import "@/index.css";
+import Ling, { createElement } from "@/packages/Ling";
 
-const root = document.getElementById("root");
+const container = document.getElementById("root");
 
-const element = createElement(
-  "div",
-  null,
-  "div",
-  createElement("img", {
-    style: "width:300px;height:300px;",
-    src: "https://cdn.pixabay.com/photo/2023/04/19/03/58/flower-7936549_1280.png",
-    alt: "img",
-  }),
-  createElement("h1", { style: "color:red" }, "red color h1")
-);
+// const element = createElement(
+//   "div",
+//   null,
+//   "div",
+//   createElement("img", {
+//     style: "width:300px;height:300px;",
+//     src: "https://cdn.pixabay.com/photo/2023/04/19/03/58/flower-7936549_1280.png",
+//     alt: "img",
+//   }),
+//   createElement("h1", { style: "color:red" }, "red color h1")
+// );
 
-Ling.render(element, root);
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
+const rerender = (value) => {
+  const element = createElement(
+    "div",
+    null,
+    createElement("input", { onInput: updateValue, value: value }),
+    createElement("h2", null, `Hello ${value}`)
+  );
+
+  Ling.render(element, container);
+};
+
+rerender("hello");
--- src/packages/Ling.jsx
+++ src/packages/Ling.jsx
@@ -20,23 +20,190 @@ export function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
-  const isProperty = (key) => key !== "children";
+  updateDom(dom, {}, fiber.props);
 
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((key) => {
-      dom[key] = element.props[key];
+  return dom;
+}
+
+let wipRoot = null;
+let currentRoot = null;
+let nextUnitOfWork = null;
+let deletions = [];
+
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+  // remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
     });
+  // remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => (dom[name] = ""));
 
-  element.props.children.forEach((child) => render(child, dom));
+  // set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => (dom[name] = nextProps[name]));
 
-  container.appendChild(dom);
+  // add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
+function commitRoot() {
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) return;
+
+  const domParent = fiber.parent.dom;
+
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom !== null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom !== null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
+
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: { children: [element] },
+    alternate: currentRoot,
+  };
+
+  deletions = [];
+  nextUnitOfWork = wipRoot;
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  // commit the fiber root to the DOM after all unit of work are cleared
+  if (!nextUnitOfWork && wipRoot) commitRoot();
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+/**
+ *
+ * 在fiber树的某个节点时, 遍历的顺序是 child > sibling > parent
+ */
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) fiber.dom = createDom(fiber);
+
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
+
+  // search the next unit of work
+  if (fiber.child) return fiber.child;
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+
+    nextFiber = nextFiber.parent;
+  }
+}
+
+function reconcileChildren(wipFiber, elements) {
+  let prevSibling = null;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+
+  // 这里的oldFiber不能使用!==
+  for (let index = 0; index < elements.length || oldFiber != null; ++index) {
+    const element = elements[index];
+    let newFiber = null;
+
+    const sameType = oldFiber && element && element.type === oldFiber.type;
+
+    /**
+     * update the node.
+     * if the old fiber and the new element have the same type,
+     * we can keep the DOM node and just update it with the new props
+     */
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+
+    /**
+     * add the node.
+     * if the type is different and there is a new element,
+     * it means we need to create a new DOM node
+     */
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    /**
+     * delete the node.
+     * if the types are different and there is an old fiber,
+     * we need to remove the old node
+     */
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) oldFiber = oldFiber.sibling;
+
+    if (index === 0) wipFiber.child = newFiber;
+    else if (element) prevSibling.sibling = newFiber;
+
+    prevSibling = newFiber;
+  }
 }
 
 const Ling = {
